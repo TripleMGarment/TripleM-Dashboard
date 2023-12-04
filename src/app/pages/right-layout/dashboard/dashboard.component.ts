5af453140e5b46de4c54c46cc83c4d72ce85a904
Vaishnavi S <vaishnavis@presidio.com>
2023-12-04 09:46:17 +0530

    Created Dashboard component

--- src/app/pages/right-layout/dashboard/dashboard.component.ts
+++ src/app/pages/right-layout/dashboard/dashboard.component.ts
@@ -1,10 +1,83 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+  topBar: any[] = [];
+  partiesList: any[] = [];
 
+  ngOnInit() {
+    this.topBar = [
+      {
+        'key': 'Parties',
+        'value': 50,
+        'route': '/parties'
+      },
+      {
+        'key': 'Orders',
+        'value': 190,
+        'route': '/orders'
+      }
+    ]
+    this.partiesList = [
+      {
+        'party_name': "Sridhar",
+        'address': "Salem, Salem",
+        'total_orders': "200"
+      },
+      {
+        'party_name': "Sridhar",
+        'address': "Salem, Salem",
+        'total_orders': "200"
+      },
+      {
+        'party_name': "Sridhar",
+        'address': "Salem, Salem",
+        'total_orders': "200"
+      },
+      {
+        'party_name': "Sridhar",
+        'address': "Salem, Salem",
+        'total_orders': "200"
+      },
+      {
+        'party_name': "Sridhar",
+        'address': "Salem, Salem",
+        'total_orders': "200"
+      },
+      {
+        'party_name': "Sridhar",
+        'address': "Salem, Salem",
+        'total_orders': "200"
+      },
+      {
+        'party_name': "Sridhar",
+        'address': "Salem, Salem",
+        'total_orders': "200"
+      },
+      {
+        'party_name': "Sridhar",
+        'address': "Salem, Salem",
+        'total_orders': "200"
+      },
+      {
+        'party_name': "Sridhar",
+        'address': "Salem, Salem",
+        'total_orders': "200"
+      },
+      {
+        'party_name': "Sridhar",
+        'address': "Salem, Salem",
+        'total_orders': "200"
+      },
+      {
+        'party_name': "Sridhar",
+        'address': "Salem, Salem",
+        'total_orders': "200"
+      }
+    ]
+  }
 }
