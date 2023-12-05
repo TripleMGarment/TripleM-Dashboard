234dfb84f554f5e89666d9c3b51e92c45567e9da
Vaishnavi S <vaishnavis@presidio.com>
2023-12-05 18:18:07 +0530

    Partially completed orders

--- src/app/pages/right-layout/orders/orders.component.ts
+++ src/app/pages/right-layout/orders/orders.component.ts
@@ -1,10 +1,95 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-orders',
   templateUrl: './orders.component.html',
   styleUrls: ['./orders.component.scss']
 })
-export class OrdersComponent {
+export class OrdersComponent implements OnInit {
+  partiesList: any[] = [];
 
+  ngOnInit() {
+    this.partiesList = [
+      {
+        'party_name': "Sridhar",
+        'address': "Salem, Salem",
+        'gst': "22AAAAA0000A1Z5"
+      },
+      {
+        'party_name': "Sridhar",
+        'address': "Salem, Salem",
+        'gst': "22AAAAA0000A1Z5"
+      },
+      {
+        'party_name': "Sridhar",
+        'address': "Salem, Salem",
+        'gst': "22AAAAA0000A1Z5"
+      },
+      {
+        'party_name': "Sridhar",
+        'address': "Salem, Salem",
+        'gst': "22AAAAA0000A1Z5"
+      },
+      {
+        'party_name': "Sridhar",
+        'address': "Salem, Salem",
+        'gst': "22AAAAA0000A1Z5"
+      },
+      {
+        'party_name': "Sridhar",
+        'address': "Salem, Salem",
+        'gst': "22AAAAA0000A1Z5"
+      },
+      {
+        'party_name': "Sridhar",
+        'address': "Salem, Salem",
+        'gst': "22AAAAA0000A1Z5"
+      },
+      {
+        'party_name': "Sridhar",
+        'address': "Salem, Salem",
+        'gst': "22AAAAA0000A1Z5"
+      },
+      {
+        'party_name': "Sridhar",
+        'address': "Salem, Salem",
+        'gst': "22AAAAA0000A1Z5"
+      },
+      {
+        'party_name': "Sridhar",
+        'address': "Salem, Salem",
+        'gst': "22AAAAA0000A1Z5"
+      },
+      {
+        'party_name': "Sridhar",
+        'address': "Salem, Salem",
+        'gst': "22AAAAA0000A1Z5"
+      },
+      {
+        'party_name': "Sridhar",
+        'address': "Salem, Salem",
+        'gst': "22AAAAA0000A1Z5"
+      },
+      {
+        'party_name': "Sridhar",
+        'address': "Salem, Salem",
+        'gst': "22AAAAA0000A1Z5"
+      },
+      {
+        'party_name': "Sridhar",
+        'address': "Salem, Salem",
+        'gst': "22AAAAA0000A1Z5"
+      },
+      {
+        'party_name': "Sridhar",
+        'address': "Salem, Salem",
+        'gst': "22AAAAA0000A1Z5"
+      },
+      {
+        'party_name': "Sridhar",
+        'address': "Salem, Salem",
+        'gst': "22AAAAA0000A1Z5"
+      }
+    ]
+  }
 }
